8af5df0543888ae8d7604b593d4a9adacf838f49
fabienAndroid <116167225+fabienAndroid@users.noreply.github.com>
2024-02-05 02:52:43 +0100

    Delete train_IAv2.py

--- train_IAv2.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8n.yaml')  # create new model
-results = model.train(data="D:/Dataset_IA/dataset_yolov8/data.yaml", epochs = 10,imgsz=[128,128], patience=5, batch=1024, save_period=1)
-model.export()
-
-# from ultralytics import YOLO
-# model = YOLO('C:/Users/fabie/OneDrive/Documents/fabien/cours/Annee_3/fil_rouge/best.pt')  # load an official model (task=segment,device=0)
-# model.export(format='engine',device=0)
